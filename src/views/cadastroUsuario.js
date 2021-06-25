13542a192ed2fd4c07d2357abc9962d96f0296fc
Felipe Ascari <felipe.ascari@gmail.com.br>
2021-06-25 14:28:01 -0300

    WIP: cadastrar usuários

--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -4,8 +4,8 @@ import { withRouter } from 'react-router-dom'
 import Card from '../components/card'
 import FormGroup from '../components/form-group'
 
-//import UsuarioService from '../app/service/usuarioService'
-//import { mensagemSucesso, mensagemErro } from '../components/toastr'
+import UsuarioService from '../app/service/usuarioService'
+import { mensagemSucesso, mensagemErro } from '../components/toastr'
 
 class CadastroUsuario extends React.Component {
 
@@ -16,32 +16,31 @@ class CadastroUsuario extends React.Component {
         senhaRepeticao: ''
     }
 
-    /*constructor(){
+    constructor() {
         super();
         this.service = new UsuarioService();
     }
 
     cadastrar = () => {
+        const { nome, email, senha, senhaRepeticao } = this.state
+        const usuario = { nome, email, senha, senhaRepeticao }
 
-        const {nome, email, senha, senhaRepeticao } = this.state        
-        const usuario = {nome,  email, senha, senhaRepeticao }
-
-        try{
+        try {
             this.service.validar(usuario);
-        }catch(erro){
+        } catch (erro) {
             const msgs = erro.mensagens;
             msgs.forEach(msg => mensagemErro(msg));
             return false;
         }
 
         this.service.salvar(usuario)
-            .then( response => {
+            .then(() => {
                 mensagemSucesso('Usuário cadastrado com sucesso! Faça o login para acessar o sistema.')
                 this.props.history.push('/login')
             }).catch(error => {
                 mensagemErro(error.response.data)
             })
-    }*/
+    }
 
     cancelar = () => {
         this.props.history.push('/login')
