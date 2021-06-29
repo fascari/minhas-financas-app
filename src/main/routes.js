3c88825a08b0f4152e31793f94ff3acaab9eafcf
Felipe Ascari <felipe.ascari@gmail.com.br>
2021-06-29 19:15:29 -0300

    Controle de exibição de menus.

--- src/main/routes.js
+++ src/main/routes.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
 
 import Login from '../views/login';
@@ -5,20 +6,42 @@ import Home from '../views/home';
 import CadastroUsuario from '../views/cadastroUsuario';
 import ConsultaLancamentos from '../views/lancamentos/consulta-lancamentos';
 import CadastroLancamentos from '../views/lancamentos/cadastro-lancamentos';
+import { AuthConsumer } from '../main/authenticationProvider'
 
-import { Route, Switch, HashRouter } from 'react-router-dom'
+import { Route, Switch, HashRouter, Redirect } from 'react-router-dom'
 
-function Routes() {
+function AuthRoute({ component: Component, isUsuarioAutenticado, ...props }) {
+    return (
+        <Route {...props} render={(componentProps) => {
+            if (isUsuarioAutenticado) {
+                return (
+                    <Component {...componentProps} />
+                )
+            } else {
+                return (
+                    <Redirect to={{ pathname: '/login', state: { from: componentProps.location } }} />
+                )
+            }
+        }} />
+    )
+}
+
+function Routes(props) {
     return (
         <HashRouter>
             <Switch>
-                <Route path="/login" component={Login}/>
-                <Route path="/home" component={Home}/>
-                <Route path="/cadastro-usuarios" component={CadastroUsuario}/>
-                <Route path="/consulta-lancamentos" component={ConsultaLancamentos}/>
-                <Route path="/cadastro-lancamentos/:id?" component={CadastroLancamentos}/>
+                <Route path="/login" component={Login} />
+                <Route path="/cadastro-usuarios" component={CadastroUsuario} />
+
+                <AuthRoute isUsuarioAutenticado={props.isUsuarioAutenticado} path="/home" component={Home} />
+                <AuthRoute isUsuarioAutenticado={props.isUsuarioAutenticado} path="/consulta-lancamentos" component={ConsultaLancamentos} />
+                <AuthRoute isUsuarioAutenticado={props.isUsuarioAutenticado} path="/cadastro-lancamentos/:id?" component={CadastroLancamentos} />
             </Switch>
-        </HashRouter>   
+        </HashRouter>
     )
 }
-export default Routes;
+export default () => (
+    <AuthConsumer>
+        {(context) => (<Routes isUsuarioAutenticado={context.isAutenticado} />)}
+    </AuthConsumer>
+)
