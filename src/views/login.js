e4132d47d425247000b213e193ba857d7d307d7c
Felipe Ascari <felipe.ascari@gmail.com.br>
2021-06-29 17:59:05 -0300

    melhorias gerais nos formulários

--- src/views/login.js
+++ src/views/login.js
@@ -64,8 +64,12 @@ class Login extends React.Component {
                                                 value={this.state.senha}
                                                 onChange={e => this.setState({ senha: e.target.value })} />
                                         </FormGroup>
-                                        <button onClick={this.entrar} className="btn btn-success">Entrar</button>
-                                        <button onClick={this.prepareCadastrar} className="btn btn-danger">Cadastrar</button>
+                                        <button onClick={this.entrar} className="btn btn-success">
+                                            <i className="pi pi-sign-in"></i> Entrar</button>
+                                        <button onClick={this.prepareCadastrar}
+                                            className="btn btn-danger">
+                                            <i className="pi pi-plus"></i>  Cadastrar
+                                        </button>
                                     </fieldset>
                                     </div>
                                 </div>
